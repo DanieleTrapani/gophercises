2712d7d2f74d8f4d8fa007043cc1fa3e4c54e14e
Daniele Trapani <daniele.trapani96@gmail.com>
2023-11-28 17:51:30 +0100

    guessing logic

--- 01-Quiz/main.go
+++ 01-Quiz/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
 	"strings"
@@ -10,17 +11,35 @@ import (
 func main() {
 	file, err := os.Open("problems.csv")
 	if err != nil {
-		log.Fatal()
+		log.Fatal(err)
+		os.Exit(1)
 	}
 	defer file.Close()
 
 	scan := bufio.NewScanner(file)
+	var lines []string
 
 	for scan.Scan() {
 		line := scan.Text()
-		println(line)
+		lines = append(lines, line)
+	}
 
+	for _, line := range lines {
 		// split the line in question and answer
 		question, answer := strings.Split(line, ",")[0], strings.Split(line, ",")[1]
+		var input string
+		fmt.Printf("What is %s?\n", question)
+		_, err := fmt.Scan(&input)
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		}
+
+		if answer == input {
+			fmt.Println("Yes!")
+		} else {
+			fmt.Println("No!")
+		}
+
 	}
 }
